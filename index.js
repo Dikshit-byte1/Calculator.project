d4efff670ad98a20db221471d14bdb39360ead73
Dikshit-byte1 <86417176+Dikshit-byte1@users.noreply.github.com>
2021-09-06 18:57:14 +0530

    Second commit "modified html and js"

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ function input(num){
     var text = document.form.number.value;
     document.form.number.value = text+num;
 }
-function mod(){
-    document.form.number.value += '%';
-}
 function square_root(){
     document.form.number.value = Math.sqrt(document.form.number.value);
 }
@@ -15,21 +12,9 @@ function clean(){
     var text = document.form.number.value;
     document.form.number.value = text.substring(0,text.length-1);
 }
-function multiply(){
-    document.form.number.value += '*';
-}
-function div(){
-    document.form.number.value += '/';
-}
 function square(){
     var text = document.form.number.value;
     document.form.number.value = Math.pow(text,2);
-}
-function sub(){
-    document.form.number.value += '-';
-}
-function add(){
-    document.form.number.value += '+';
 }
 function plus_minus(){
     var text = document.form.number.value;
